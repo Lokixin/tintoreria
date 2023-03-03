b4a591cd178717de615129dddae5e3081868aa9c
Lokixin <dimasavilamartinez@gmail.com>
2023-03-03 20:55:30 +0100

    Afegida una pantalla per registrar un nou usuari

--- src/use_cases/views.py
+++ src/use_cases/views.py
@@ -12,9 +12,9 @@ class MainScreen(tk.Frame):
         self.init_widgets()
 
     def init_widgets(self):
-        tk.Label(
-            self, text="Crea o realiza tests", justify=tk.CENTER, **styles.STYLE
-        ).pack(**styles.PACK)
+        tk.Label(self, text="Hola Hola", justify=tk.CENTER, **styles.STYLE).pack(
+            **styles.PACK
+        )
 
 
 class AddCP(tk.Frame):
@@ -73,3 +73,95 @@ class AddCP(tk.Frame):
     def update_locality(self, *args) -> None:
         locality = get_locality_by_cp(cp=self.postal_code.get())
         self.locality.set(locality)
+
+
+class AddClient(tk.Frame):
+    def __init__(self, parent, manager):
+        super().__init__(parent)
+        self.manager = manager
+        self.configure(background=styles.BACKGROUND)
+        self.name_var = tk.StringVar(self)
+        self.surname_var = tk.StringVar(self)
+        self.phone_var = tk.StringVar(self)
+        self.init_widgets()
+
+    def save_new_client(self):
+        name = self.name_var.get()
+        surname = self.surname_var.get()
+        phone = self.phone_var.get()
+        print(f"Nou client: {name=} - {surname=} - {phone=}")
+
+    def init_widgets(self):
+        tk.Label(
+            self, text="Aquí añadiremos clientes", justify=tk.CENTER, **styles.STYLE
+        ).pack(**styles.PACK)
+
+        form_frame = tk.Frame(self)
+        form_frame.configure(background=styles.BACKGROUND)
+        form_frame.pack(
+            side=tk.TOP,
+            fill=tk.BOTH,
+            expand=True,
+            padx=5,
+            pady=5,
+        )
+        form_frame.grid_columnconfigure(index=1, weight=3)
+        form_frame.rowconfigure(index=0, weight=1)
+
+        tk.Label(form_frame, text="Nom: ", justify=tk.CENTER, **styles.STYLE).grid(
+            column=0,
+            row=0,
+            padx=5,
+            pady=5,
+            sticky=tk.EW,
+        )
+
+        tk.Entry(form_frame, textvariable=self.name_var, justify=tk.CENTER, **styles.STYLE).grid(
+            column=1,
+            row=0,
+            padx=5,
+            pady=5,
+            sticky=tk.EW,
+        )
+
+        tk.Label(form_frame, text="Cognom: ", justify=tk.CENTER, **styles.STYLE).grid(
+            column=0,
+            row=1,
+            padx=5,
+            pady=5,
+            sticky=tk.EW,
+        )
+
+        tk.Entry(form_frame, textvariable=self.surname_var, justify=tk.CENTER, **styles.STYLE).grid(
+            column=1,
+            row=1,
+            padx=5,
+            pady=5,
+            sticky=tk.EW,
+        )
+
+        tk.Label(form_frame, text="Telèfon: ", justify=tk.CENTER, **styles.STYLE).grid(
+            column=0,
+            row=2,
+            padx=5,
+            pady=5,
+            sticky=tk.EW,
+        )
+
+        tk.Entry(form_frame, textvariable=self.phone_var, justify=tk.CENTER, **styles.STYLE).grid(
+            column=1,
+            row=2,
+            padx=5,
+            pady=5,
+            sticky=tk.EW,
+        )
+
+        tk.Button(
+            self,
+            text="Guardar cliente",
+            relief=tk.FLAT,
+            activebackground=styles.BACKGROUND,
+            activeforeground=styles.TEXT,
+            command=self.save_new_client,
+            **styles.STYLE,
+        ).pack(**styles.PACK)
